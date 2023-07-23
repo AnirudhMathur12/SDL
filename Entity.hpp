69206662ac7bd7eeb2dbd70ee4e5e3f69a1bc3dd
AnirudhMathur12 <126768505+AnirudhMathur12@users.noreply.github.com>
2023-07-23 19:47:29 +0530

    Create Entity.hpp and Entity.cpp

---
+++ Entity.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <SDL2/SDL.h>
+
+class Entity
+{
+    SDL_Rect position;
+    SDL_Texture tex;
+    
+    Entity()
+    {
+        std::cout << "yay!";
+    }
+};
